dcba2f102ce1eb037a675de287e72aaada74c289
happyAnger6 <happyAnger6@163.com>
2017-01-25 05:09:11 -0800

    add before spring

--- webapp/extensions.py
+++ webapp/extensions.py
@@ -1,3 +1,16 @@
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 bcrypt = Bcrypt()
+
+login_manager = LoginManager()
+
+login_manager.login_view = "main.login"
+login_manager.session_protection = "strong"
+login_manager.login_message = "Please login to access this page"
+login_manager.login_message_category = "info"
+
+@login_manager.user_loader
+def load_user(userid):
+    from .models import User
+    return User.query.get(userid)
